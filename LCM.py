9de1e5962cd61e86816c2ca5527e7034ccb6c4e9
Sivaram18162 <97424725+Sivaram18162@users.noreply.github.com>
2022-06-29 18:39:54 +0530

    a,b=map(int,input().split()) if(a>b):     a,b=b,a for i in range(1,b+1):     if(a%i==0 and b%i==0):         gcd=i lcm=a*b//gcd print(lcm)

--- LCM.py
+++ LCM.py
@@ -1,8 +1,8 @@
-def lcm(a,b):
-    c=b
-    while True:
-        if c%a==0 and c%b==0:
-            return c
-        c=c+1
 a,b=map(int,input().split())
-print(lcm(a,b))
+if(a>b):
+    a,b=b,a
+for i in range(1,b+1):
+    if(a%i==0 and b%i==0):
+        gcd=i
+lcm=a*b//gcd
+print(lcm)
